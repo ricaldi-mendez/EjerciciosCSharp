49485dca804b33b22e18e4f6a9ae47c7f412123a
Jhordan Angelo <Ricaldi_Mendez@hotmail.com>
2023-02-17 21:16:16 +0000

    añadiendo proyecto conexion BD

---
+++ ConexionBD/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+// See https://aka.ms/new-console-template for more information
+
+
+SqlConnection con= new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;
+AttachDbFilename=C:\Users\jhordan\source\repos\ConexionBD\Database1.mdf;
+Integrated Security=True");
+
+con.Open();
+Console.WriteLine(con.State);
+
+String query = "SELECT * FROM Pokemones";
+SqlCommand comand = new SqlCommand(query,con);
+var resultado = comand.ExecuteReader();
+while (resultado.Read()) {
+    Console.WriteLine(resultado["nombre"]);
+}
+
+
+Console.WriteLine("Holaaa");
+con.Close();
